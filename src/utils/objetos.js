3525ac344a9db905fb664c4a9330f64dc87fe286
Raulez3104 <raulez3104@hotmail.com>
2025-10-01 19:48:49 -0400

    Segundo Commit

--- src/utils/objetos.js
+++ src/utils/objetos.js
@@ -1,16 +1,9 @@
-// Funciones para manejar objetos perdidos
-
-// Agregar un objeto al arreglo
 export function agregarObjeto(objetos, nuevoObjeto) {
   return [...objetos, nuevoObjeto];
 }
-
-// Buscar un objeto por nombre
 export function buscarObjeto(objetos, nombre) {
   return objetos.filter(obj => obj.nombre.toLowerCase() === nombre.toLowerCase());
 }
-
-// Marcar un objeto como encontrado
 export function marcarEncontrado(objetos, id) {
   return objetos.map(obj => obj.id === id ? { ...obj, encontrado: true } : obj);
 }
